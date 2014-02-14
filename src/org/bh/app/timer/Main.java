9ac153df3a68538266a79d985d13cf01ad26bbf8
Supuhstar <supuhstar0@gmail.com>
2014-02-14 17:19:15 -0500

    Test

--- src/org/bh/app/timer/Main.java
+++ src/org/bh/app/timer/Main.java
@@ -30,7 +30,6 @@ public class Main
 	public static final String APP_NAME = "Blue Husky's Timer 2";
 	public static final String APP_ABBR = new StringPP(APP_NAME).toAbbreviation(true)+"";
 	public static final String VERSION = "2.0.0\u03BB";
-	
 	private static final Logger LOGGER = Logger.getLogger(Main.class.getCanonicalName());
 	private static boolean devMode = false;
 	
